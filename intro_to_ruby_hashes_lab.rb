d4731de3eb9ec43d7bfee049e5869f565bab3d43
Ebony Sharrock <ebonysharrock@gmail.com>
2019-10-16 14:49:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key]  hash[key] += 1
-  else hash.default(1)
+  if hash[key]  
+    hash[key] += 1
+  else 
+    hash.default(1)
   end
   p hash
 end
